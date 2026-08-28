1d395e7e415d4b9593a086b50368a34ee3174638
s4149976 <s4149976@rmit.edu.vn>
2026-08-29 00:46:32 +0700

    Update sliderData.js

--- sliderData.js
+++ sliderData.js
@@ -14,5 +14,4 @@ export const personalSliderData = [
   { title: "[FANART]", img: "./images/kaneki.png"},
   { title: "[FANART]", img: "./images/Superman.png"},
   { title: "[FANART]", img: "./images/COTN.png"},
-  { title: "[FANART]", img: "./images/Phoster Rokuhira.png"},
 ];
